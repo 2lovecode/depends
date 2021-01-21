14cfd9306e659a30f7289135a0e9c8923f0710ab
2lovecode <liu546hao@163.com>
2021-01-21 15:23:45 +0800

    并行依赖模型

---
+++ depends.go
@@ -0,0 +1,237 @@
+package depends
+
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"sync/atomic"
+	"time"
+)
+
+type IService interface {
+	Name() string
+	Run(ctx context.Context) error
+	Decode(receiver interface{}) error
+}
+
+type Depends struct {
+	serviceMap      map[string]IService // 服务map
+	serviceCount    int32
+	depends         map[string][]string // 依赖关系
+	starts          []string            //第一波调用
+	inversedDepends map[string][]string // 反依赖关系
+	executeFlags    []int32
+	executeMap      map[string]*int32 // 执行列表 每一个服务对应一条记录，记录中：0 - 未执行 1 - 执行完毕
+	executecStatus  int32             // 总体进度表 0 - 未结束 1 - 已结束
+	inChanels       map[string]chan bool
+	outChanel       chan string
+	ctrlCtx         context.Context
+	ctrlCancel      context.CancelFunc
+	timeout         time.Duration
+}
+
+func NewDepends(timeout time.Duration) *Depends {
+	if timeout == 0 {
+		timeout = 1000 * time.Millisecond
+	}
+	return &Depends{
+		serviceMap:      make(map[string]IService, 0),
+		serviceCount:    0,
+		depends:         make(map[string][]string, 0),
+		inversedDepends: make(map[string][]string, 0),
+		executeFlags:    make([]int32, 500),
+		executeMap:      make(map[string]*int32),
+		executecStatus:  0,
+		timeout:         timeout,
+	}
+}
+
+// 注册服务
+func (me *Depends) Register(service IService) error {
+	me.serviceMap[service.Name()] = service
+	me.executeMap[service.Name()] = &me.executeFlags[int(me.serviceCount)]
+	me.serviceCount++
+	// todo 重复注册等情况的处理
+	return nil
+}
+
+// 添加依赖关系
+func (me *Depends) AddDepend(service IService, serviceDepends []IService) error {
+	var dependNames []string
+	for _, item := range serviceDepends {
+		dependNames = append(dependNames, item.Name())
+	}
+	if _, ok := me.depends[service.Name()]; ok {
+		me.depends[service.Name()] = append(me.depends[service.Name()], dependNames...)
+	} else {
+		me.depends[service.Name()] = dependNames
+	}
+	//todo 依赖检查等情况的处理
+	return nil
+}
+
+// 执行
+func (me *Depends) Execute(ctx context.Context) {
+	timeOutCtrl := time.After(me.timeout)
+	// 输入输出通道
+	me.inChanels = make(map[string]chan bool)
+	me.outChanel = make(chan string)
+	defer func() {
+		atomic.StoreInt32(&(me.executecStatus), 1)
+		close(me.outChanel)
+
+		for _, in := range me.inChanels {
+			close(in)
+		}
+	}()
+	// 初始化 解析出反依赖关系和第一波调用队列
+	me.bootstrap()
+
+	// 点火
+	me.fire()
+
+	// 调度
+	me.dispatch()
+
+	// 运行
+	me.operate(ctx)
+
+	tick := time.Tick(1 * time.Millisecond)
+	for {
+		breakFlag := false
+		select {
+		case <-timeOutCtrl:
+			atomic.StoreInt32(&me.executecStatus, 1)
+			breakFlag = true
+		case <-tick:
+			finishExecuteCount := 0
+			for _, flag := range me.executeMap {
+				if atomic.LoadInt32(flag) == 1 {
+					finishExecuteCount++
+				}
+			}
+			if finishExecuteCount >= int(me.serviceCount) {
+				atomic.StoreInt32(&me.executecStatus, 1)
+				breakFlag = true
+			}
+		}
+
+		if breakFlag {
+			break
+		}
+	}
+}
+
+// 初始化
+func (me *Depends) bootstrap() error {
+	me.ctrlCtx, me.ctrlCancel = context.WithTimeout(context.Background(), 2000*time.Millisecond)
+
+	for _, eachService := range me.serviceMap {
+		me.inChanels[eachService.Name()] = make(chan bool)
+
+		if depends, ok := me.depends[eachService.Name()]; !ok {
+			me.starts = append(me.starts, eachService.Name())
+		} else {
+			for _, dp := range depends {
+				if _, o := me.inversedDepends[dp]; o {
+					me.inversedDepends[dp] = append(me.inversedDepends[dp], eachService.Name())
+				} else {
+					me.inversedDepends[dp] = []string{eachService.Name()}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// 启动第一波调用
+func (me *Depends) fire() error {
+	tGo(func() error {
+		for _, startName := range me.starts {
+			me.inChanels[startName] <- true
+		}
+		return nil
+	})
+
+	return nil
+}
+
+// 调度
+func (me *Depends) dispatch() error {
+	tGo(func() error {
+		for serviceName := range me.outChanel {
+			if serviceName != "" {
+				executeQueue := make([]string, 0)
+				if atomic.LoadInt32(&me.executecStatus) != 1 {
+					if inversedDepends, ok := me.inversedDepends[serviceName]; ok {
+						for _, inversedDepend := range inversedDepends {
+							// 检查依赖项
+							flag := true
+							if depends, o := me.depends[inversedDepend]; o {
+								for _, depend := range depends {
+									if atomic.LoadInt32(me.executeMap[depend]) != 1 {
+										flag = false
+										break
+									}
+								}
+							}
+							if flag {
+								executeQueue = append(executeQueue, inversedDepend)
+							}
+						}
+					}
+				}
+
+				if len(executeQueue) > 0 {
+					for _, eachName := range executeQueue {
+						if inChanel, ok := me.inChanels[eachName]; ok {
+							inChanel <- true
+						}
+					}
+				}
+			}
+		}
+		return nil
+	})
+	return nil
+}
+
+func (me *Depends) operate(ctx context.Context) error {
+	for _, service := range me.serviceMap {
+
+		go func(in chan bool, s IService) {
+			defer func() {
+				// 错误处理
+				if p := recover(); p != nil {
+					fmt.Println(debug.Stack())
+				}
+			}()
+			if flag, open := <-in; flag && open {
+				tNow := time.Now()
+				// 执行
+				s.Run(ctx)
+				atomic.StoreInt32(me.executeMap[s.Name()], 1)
+				eNow := time.Now()
+				fmt.Println(s.Name(), "执行", eNow.Sub(tNow).Milliseconds())
+				if atomic.LoadInt32(&me.executecStatus) != 1 {
+					me.outChanel <- s.Name()
+				}
+			}
+
+		}(me.inChanels[service.Name()], service)
+	}
+	return nil
+}
+
+func tGo(fn func() error) {
+	go func() {
+		defer func() {
+			// 错误处理
+			if p := recover(); p != nil {
+				fmt.Println(debug.Stack())
+			}
+		}()
+		fn()
+	}()
+}
---
+++ depends_test.go
@@ -0,0 +1,134 @@
+package depends
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+type ServiceAData struct {
+	Message string
+	Query string
+}
+type ServiceA struct {
+	data ServiceAData
+}
+
+func NewServiceA() *ServiceA {
+	return &ServiceA{}
+}
+
+func (s *ServiceA) Name() string {
+	return "service_a"
+}
+
+func (s * ServiceA) Run(ctx context.Context) error {
+	s.data = ServiceAData{
+		Message:"I am service a",
+		Query: ctx.Value("q").(string),
+	}
+	return nil
+}
+
+func (s * ServiceA) Decode(receiver interface{}) error {
+	if _, ok := receiver.(*ServiceAData); ok {
+		*(receiver.(*ServiceAData)) = s.data
+	}
+	return nil
+}
+
+
+type ServiceBData struct {
+	Message string
+	Query string
+}
+type ServiceB struct {
+	data ServiceBData
+}
+
+func NewServiceB() *ServiceB {
+	return &ServiceB{}
+}
+
+func (s *ServiceB) Name() string {
+	return "service_b"
+}
+
+func (s * ServiceB) Run(ctx context.Context) error {
+	s.data = ServiceBData{
+		Message:"I am service b",
+		Query: ctx.Value("q").(string),
+	}
+	return nil
+}
+
+func (s * ServiceB) Decode(receiver interface{}) error {
+	if _, ok := receiver.(*ServiceBData); ok {
+		*(receiver.(*ServiceBData)) = s.data
+	}
+	return nil
+}
+
+
+type ServiceCData struct {
+	Message string
+	Query string
+}
+type ServiceC struct {
+	data ServiceCData
+}
+
+func NewServiceC() *ServiceC {
+	return &ServiceC{}
+}
+
+func (s *ServiceC) Name() string {
+	return "service_c"
+}
+
+func (s * ServiceC) Run(ctx context.Context) error {
+	s.data = ServiceCData{
+		Message:"I am service c",
+		Query: ctx.Value("q").(string),
+	}
+	return nil
+}
+
+func (s * ServiceC) Decode(receiver interface{}) error {
+	if _, ok := receiver.(*ServiceCData); ok {
+		*(receiver.(*ServiceCData)) = s.data
+	}
+	return nil
+}
+
+func TestDepends_Execute(t *testing.T) {
+	ctx := context.WithValue(context.TODO(), "q", "test")
+
+	sA := NewServiceA()
+	sB := NewServiceB()
+	sC := NewServiceC()
+
+	hd := NewDepends(100 * time.Millisecond)
+
+	hd.Register(sA)
+	hd.Register(sB)
+	hd.Register(sC)
+
+
+	hd.AddDepend(sC, []IService{sB})
+	hd.AddDepend(sB, []IService{sA})
+
+	hd.Execute(ctx)
+
+	sAData := ServiceAData{}
+	sBData := ServiceBData{}
+	sCData := ServiceCData{}
+
+
+	sA.Decode(&sAData)
+	sB.Decode(&sBData)
+	sC.Decode(&sCData)
+
+	fmt.Println(sAData, sBData, sCData)
+}
